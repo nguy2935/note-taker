27c2ae42d2eca703acc6cf2581514266c1abfbaf
unknown <Sophia Nguyen>
2022-03-14 04:13:40 -0500

    updated server.js to get app to route to html files

--- server.js
+++ server.js
@@ -11,6 +11,15 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.use(express.json());
 
+app.get("/notes", (req,res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
 app.listen(PORT, () => {
     console.log(`App API is live on port ${PORT}.`);
 });
+
